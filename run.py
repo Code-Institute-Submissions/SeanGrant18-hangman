a69c8dda1feec2af47722a93ef1ee69194bd00bd
SeanGrant18 <sgseangrant@gmail.com>
2022-04-28 14:34:03 +0000

    AdFunction That Randomly Selects A Word From The words.py File

---
+++ run.py
@@ -0,0 +1,10 @@
+import random
+from words import words
+import string
+
+def get_valid_word(words):
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
